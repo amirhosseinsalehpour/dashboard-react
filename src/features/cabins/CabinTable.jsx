effae69bfe32226e47d90751cd081a47a6f5136d
AmirHossein Salehpour <salehpouramirhossein19@gmail.com>
2024-02-03 11:18:45 +0330

    Client-Side Filtering Filtering Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,25 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import styled from "styled-components";
 import { getCabiens } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
 function CabinTable() {
   const {
     isLoading,
@@ -29,6 +15,17 @@ function CabinTable() {
     queryKey: ["carbin"],
     queryFn: getCabiens,
   });
+
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("discount") || "all";
+  let filteredCabin;
+  if (filterValue === "all") filteredCabin = cabins;
+  if (filterValue === "no-discount")
+    filteredCabin = cabins.filter((cabin) => cabin.discount === 0);
+
+  if (filterValue === "with-discount")
+    filteredCabin = cabins.filter((cabin) => cabin.discount > 0);
 
   if (isLoading) return <Spinner />;
 
@@ -43,7 +40,7 @@ function CabinTable() {
           <div>Discount</div>
           <div></div>
         </Table.Header>
-        {cabins.map((cabin) => (
+        {filteredCabin.map((cabin) => (
           <CabinRow cabin={cabin} key={cabin.id} />
         ))}
       </Table>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,13 +2,14 @@ import Row from "../ui/Row";
 import Heading from "../ui/Heading";
 import CabinTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filtter / Sort</p>
+        <CabinTableOperations />
       </Row>
       <Row>
         <CabinTable />
