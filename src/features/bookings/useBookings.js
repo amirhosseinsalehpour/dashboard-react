5025295e2130ade962cbc05dbc7db59311e814c3
AmirHossein Salehpour <salehpouramirhossein19@gmail.com>
2024-02-05 14:24:39 +0330

    API-Side Filtering Filtering Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -4,20 +4,26 @@ import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
   const [searchParams] = useSearchParams();
-
+  // Filter
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
+  // Sort
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+
+  const [field, direction] = sortByRaw.split("-");
+
+  const sortBy = { field, direction };
 
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings",filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, bookings, error };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -7,11 +7,17 @@ export async function getBookings({ filter, sortBy }) {
     .select(
       "id , created_at , startDate , endDate, numNights, numGuests, status , totalPrice, cabins(name), guests(fullName , email)"
     );
-      
-    // FILTER
-  if (filter !== null) query = query.eq(filter.field, filter.value);  
+
+  // FILTER
+  if (filter) query = query.eq(filter.field, filter.value);
   if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
-  
+
+  // SORT
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+
   const { data, error } = await query;
 
   if (error) {
